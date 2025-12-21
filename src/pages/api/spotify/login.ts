b523e9946f54716546280bbbbcd46669fe128da3
boonyanone <boonyanone@gmail.com>
2025-12-21 20:54:37 +0700

    fix: restore process.env variables (security compliance)

--- src/pages/api/spotify/login.ts
+++ src/pages/api/spotify/login.ts
@@ -1,8 +1,8 @@
 // pages/api/login.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const clientId = process.env.SPOTIFY_CLIENT_ID || "be495e578f89486e9d3c8ca7be1b1e27";
-const redirectUri = "https://playyouoke.vercel.app/api/spotify/callback";
+const clientId = process.env.SPOTIFY_CLIENT_ID!;
+const redirectUri = process.env.SPOTIFY_REDIRECT_URI!;
 const scopes = "playlist-read-private playlist-read-collaborative";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
